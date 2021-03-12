6ea9ed1fd77664ce53e75c80e509cbaa7c930f86
CodyAdam <cody.ad4@gmail.com>
2021-03-12 16:07:36 +0100

    Created Question Card component

---
+++ components/QuestionCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex-col">
+    <div class="text-5xl font-bold text-black p-5">
+      {{ question }}
+    </div>
+    <div class="w-full flex justify-center">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'question-card',
+  props: {
+    question: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
