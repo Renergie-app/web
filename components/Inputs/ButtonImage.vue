5dc95d525ed6bdb97c885a9c48385ab8eb32199e
CodyAdam <cody.ad4@gmail.com>
2021-03-12 16:07:27 +0100

    Created ButtonImage component

---
+++ components/Inputs/ButtonImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="m-3 p-2 flex-col border-2 border-black">
+    <img :src="img" :alt="title" class="h-32 w-32" />
+    <div class="text-black text-lg p-3">{{ title }}</div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'button-image',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
