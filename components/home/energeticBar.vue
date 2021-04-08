556af0acdfaba6dd8a9ccc3ba9a70d946c211e6c
CodyAdam <cody.ad4@gmail.com>
2021-04-08 20:29:18 +0200

    Stylize the energetic label component

---
+++ components/home/energeticBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flex flex-row items-center">
+    <dir
+      class="h-8 bar p-0 mt-1 mb-1 flex items-center justify-start"
+      :style="barStyle"
+    >
+      <span class="ml-4 font-extrabold text-white">
+        {{ label }}
+      </span>
+    </dir>
+    <svg
+      v-if="current"
+      :class="current ? 'current' : ''"
+      class="h-8 ml-2"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+    >
+      <path
+        d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"
+      />
+    </svg>
+  </div>
+</template>
+
+<style>
+.bar {
+  border-radius: 10px;
+}
+svg {
+  --arrow: rgb(73, 73, 73);
+  fill: var(--arrow);
+  stroke: var(--arrow);
+}
+@keyframes slideinout {
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(20px, 0);
+  }
+}
+.current {
+  animation: 2s ease-in-out 1s infinite alternate both slideinout;
+}
+</style>
+<script>
+export default {
+  name: 'energetic-bar',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    percent: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    current: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    barStyle() {
+      return {
+        'background-color': this.color,
+        width: this.percent * 0.8 + '%',
+      }
+    },
+  },
+}
+</script>
--- components/home/energeticLabel.vue
+++ components/home/energeticLabel.vue
@@ -1,47 +1,83 @@
 <template>
-  <div>
-    <table style="width:500px">
-      <tr>
-        <td> <div style="width:15%; background-color: #4CA531; float: left;"> <p> A </p> </div> <p v-if="value<=50"> <-- Actuellement </p> </td>
-      </tr>
-      <tr>
-        <td><div style="width:20%; background-color: #69B433; float: left;"> <p> B </p> </div> <p v-if="value>50 && value<=90"> <-- Actuellement </p></td>
-      </tr>
-      <tr>
-        <td><div style="width:25%; background-color: #C9D600; float: left;"> <p> C </p> </div> <p v-if="value>90 && value<=150"> <-- Actuellement </p></td>
-      </tr>
-      <tr>
-        <td><div style="width:30%; background-color: #FCEC23; float: left;"> <p> D </p> </div> <p v-if="value>150 && value<=230"> <-- Actuellement </p></td>
-      </tr>
-      <tr>
-        <td><div style="width:35%; background-color: #F6C100; float: left;"> <p> E </p> </div> <p v-if="value>230 && value<=330"> <-- Actuellement </p></td>
-      </tr>
-      <tr>
-        <td><div style="width:40%; background-color: #EC890A; float: left;"> <p> F </p> </div> <p v-if="value>330 && value<=450"> <-- Actuellement </p></td>
-      </tr>
-      <tr>
-        <td><div style="width:45%; background-color: #DB0425; float: left;"> <p> G </p> </div> <p v-if="value>450"> <-- Actuellement </p> </td>
-      </tr>
-    </table>
-    <p> Note : votre étiquette est calculée à l'aide d'un calcul théorique de votre consommation kWhep.</p>
+  <div class="w-60 mt-5">
+    <energetic-bar
+      :label="'A'"
+      :current="value <= 50"
+      :percent="25"
+      :color="'#9AE077'"
+    />
+    <energetic-bar
+      :label="'B'"
+      :current="value > 50 && value <= 90"
+      :percent="37.5"
+      :color="'#B6E075'"
+    />
+    <energetic-bar
+      :label="'C'"
+      :current="value > 90 && value <= 150"
+      :percent="50"
+      :color="'#E0DE7B'"
+    />
+    <energetic-bar
+      :label="'D'"
+      :current="value > 150 && value <= 230"
+      :percent="62.5"
+      :color="'#E0D07C'"
+    />
+    <energetic-bar
+      :label="'E'"
+      :current="value > 230 && value <= 330"
+      :percent="75"
+      :color="'#E0BC79'"
+    />
+    <energetic-bar
+      :label="'F'"
+      :current="value > 330 && value <= 450"
+      :percent="87.5"
+      :color="'#E0A374'"
+    />
+    <energetic-bar
+      :label="'G'"
+      :current="value > 450"
+      :percent="100"
+      :color="'#E08363'"
+    />
+    <div class="flex flex-row items-center">
+      <svg
+        class="h-16 mr-3 opacity-30"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <path
+          d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm0-338c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
+        />
+      </svg>
+      <p class="note text-sm mt-3 opacity-60">
+        Votre étiquette est calculée à l'aide d'un calcul théorique de votre
+        consommation kWhep
+      </p>
+    </div>
   </div>
 </template>
+<style scoped>
+.note {
+  color: var(--text-sec);
+}
+</style>
 
 <script>
+import energeticBar from './energeticBar.vue'
 export default {
-  name: "energeticLabel",
+  components: { energeticBar },
+  name: 'energeticLabel',
 
-  computed:{
-    value(){
-      return this.elecConsumption * 2.58 / this.homeArea;
-    }
+  computed: {
+    value() {
+      return (this.elecConsumption * 2.58) / this.homeArea
+    },
   },
 
-  props:["elecConsumption", "homeArea"],
-
+  props: ['elecConsumption', 'homeArea'],
 }
 </script>
 
-<style scoped>
-
-</style>
