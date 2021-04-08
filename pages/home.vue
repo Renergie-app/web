965452ab561643b607978ac6d094f21184bb8c47
CodyAdam <cody.ad4@gmail.com>
2021-04-08 20:27:10 +0200

    Extract info text from infobox component

--- pages/home.vue
+++ pages/home.vue
@@ -1,12 +1,24 @@
 <template>
   <page-container>
-    <info-box></info-box>
-    <energetic-label
-      v-if="userInfo != null"
-      :home-area="userInfo.homeArea.value"
-      :elecConsumption="userInfo.elecConsumptionTotal.value"
-    ></energetic-label>
-    <user-info-box :user-info="userInfo"></user-info-box>
+    <div class="flex flex-row">
+      <user-info-box :user-info="userInfo">
+        <energetic-label
+          v-if="userInfo != null"
+          :home-area="userInfo.homeArea.value"
+          :elecConsumption="userInfo.elecConsumptionTotal.value"
+        ></energetic-label>
+      </user-info-box>
+      <info-box>
+        Cette application s'appuie sur des données prévisionnelles et des
+        données statistiques pour prévoir vos coûts. Il est donc possible que
+        <b
+          >d'un particulier à l'autres les différences entre la réalité soit
+          plus grande</b
+        >. Cette application n'a pas pour but de remplacer un quelconque devis
+        fait par un professionnel, elle a pour seul intérêt de donner un ordre
+        d'idées de vos investissements et de leur rentabilité.
+      </info-box>
+    </div>
     <h1>Choississez votre rénovation :</h1>
     <div class="w-screen flex items-center justify-center">
       <button-image title="Eolien" img="/icon.png"> </button-image>
