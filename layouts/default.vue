48174c125d21df87eb6d57aba51f286e6ec31956
CodyAdam <cody.ad4@gmail.com>
2021-04-08 14:13:40 +0200

    Change global BG color

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,7 @@ html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
+  background-color: #eaedf2;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
