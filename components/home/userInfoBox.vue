420ae5fef00368609f2dc65aa3f496f770eea5cf
CodyAdam <cody.ad4@gmail.com>
2021-04-08 18:10:56 +0200

    Stylize the userInfoBox component

--- components/home/userInfoBox.vue
+++ components/home/userInfoBox.vue
@@ -1,16 +1,59 @@
 <template>
-  <div style="background-color: green; width: 500px">
-    <p v-if="userInfo != null">
-      Votre logement : {{ userInfo.homeType.value }} <br />
-      Votre code Postal : {{ userInfo.postalCode.value }} <br />
-      La surface de votre logement : {{ userInfo.homeArea.value }} m² <br />
-      Votre consommation éléctrique :
-      {{ userInfo.elecConsumptionTotal.value }} kWh <br />
-    </p>
-    <NextButton text="Réentrer mes infos" @clicked="goBack"> </NextButton>
+  <div class="outer p-7 m-10">
+    <h1 class="font-bold text-3xl mb-2">Mon profil</h1>
+
+    <div v-if="userInfo != null" class="mb-5">
+      <p>
+        Type de logement :
+        <span class="info font-semibold">
+          {{ userInfo.homeType.value }}
+        </span>
+      </p>
+      <p>
+        Code postal :
+        <span class="info font-semibold">
+          {{ userInfo.postalCode.value }}
+        </span>
+      </p>
+      <p>
+        Surface du logement :
+        <span class="info font-semibold">
+          {{ userInfo.homeArea.value }}
+          m²
+        </span>
+      </p>
+      <p>
+        Consomation éléctrique :
+        <span class="info font-semibold">
+          {{ userInfo.elecConsumptionTotal.value }}
+          kWh
+        </span>
+      </p>
+    </div>
+    <div v-else>
+      <h3 class="text-xl font-semibold" style="color: var(--red)">
+        Information non trouvé
+      </h3>
+    </div>
+
+    <NextButton text="Changer mes informations" @clicked="goBack"> </NextButton>
   </div>
 </template>
 
+<style scoped>
+.info {
+  color: var(--red);
+}
+p {
+  color: var(--text-sec);
+  font-weight: 500;
+}
+.outer {
+  border-radius: 40px;
+  background-color: var(--bg-card);
+  box-shadow: 0px 10px 60px -10px rgba(49, 49, 49, 0.247);
+}
+</style>
 <script>
 export default {
   name: 'userInfoBox',
@@ -23,5 +66,3 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
