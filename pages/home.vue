a2dda9a3b057809e7b77e77b952813f8b2259c4b
CodyAdam <cody.ad4@gmail.com>
2021-04-08 13:12:16 +0200

    Indent + Change tailwinds class

--- pages/home.vue
+++ pages/home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="mt-5 h-screen w-screen items-center justify-center">
+  <div class="mt-5 container h-screen w-screen items-center justify-center">
     <info-box></info-box>
-    <energetic-label v-if="userInfo!=null" :home-area="userInfo.homeArea.value" :elecConsumption="userInfo.elecConsumptionTotal.value"></energetic-label>
+    <energetic-label
+      v-if="userInfo != null"
+      :home-area="userInfo.homeArea.value"
+      :elecConsumption="userInfo.elecConsumptionTotal.value"
+    ></energetic-label>
     <user-info-box :user-info="userInfo"></user-info-box>
-    <h1> Choississez votre rénovation : </h1>
+    <h1>Choississez votre rénovation :</h1>
     <div class="w-screen flex items-center justify-center">
-      <button-image title="Eolien" img="/icon.png" > </button-image>
-      <button-image title="Panneaux Photovoltaïques" img="/icon.png" @click="solarRenov"> </button-image>
+      <button-image title="Eolien" img="/icon.png"> </button-image>
+      <button-image
+        title="Panneaux Photovoltaïques"
+        img="/icon.png"
+        @click="solarRenov"
+      >
+      </button-image>
       <button-image title="Isolation" img="/icon.png"> </button-image>
     </div>
   </div>
@@ -14,25 +23,27 @@
 
 <script>
 export default {
-  name: "home.vue",
+  name: 'home.vue',
 
   data: () => ({
     userInfo: null,
   }),
 
-  methods:{
-    solarRenov(){
-      this.$router.push("/solar");
+  methods: {
+    solarRenov() {
+      this.$router.push('/solar')
     },
   },
 
   mounted() {
-    this.userInfo = this.$store.getters["getUserInfo"]
-    if(this.userInfo==null) this.$router.push("/")
-  }
+    this.userInfo = this.$store.getters['getUserInfo']
+    if (this.userInfo == null) this.$router.push('/')
+  },
 }
 </script>
 
 <style scoped>
-
+.container {
+  background-color: #eaedf2;
+}
 </style>
