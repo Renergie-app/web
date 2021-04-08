5cfdcfc3dd2c7bc7ad515d727036879456074451
CodyAdam <cody.ad4@gmail.com>
2021-04-08 13:10:21 +0200

    Rename the SingleButton to NextButton
    TODO : create a PrevButton

---
+++ components/Inputs/NextButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    class="m-3 p-2 flex-col border-2 border-black"
+    @click="click"
+    :style="{ color: colorBtn }"
+    :disabled="disabled"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'NextButton',
+  props: ['text', 'enable'],
+
+  computed: {
+    disabled() {
+      return this.enable != undefined && !this.enable
+    },
+    colorBtn() {
+      return this.disabled ? 'red' : 'black'
+    },
+  },
+
+  methods: {
+    click() {
+      this.$emit('clicked')
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
