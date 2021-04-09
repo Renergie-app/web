fe6c6903a5d48900b0c9ded1e9efc686b561fd83
CodyAdam <cody.ad4@gmail.com>
2021-04-09 12:41:41 +0200

    Add content to the bilan panel

--- components/bilanPanel.vue
+++ components/bilanPanel.vue
@@ -1,8 +1,21 @@
-<template></template>
+<template>
+  <div class="outer bg-white p-5 rounded-3xl shadow-2xl">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 700;
+  font-size: 1.875rem /* 30px */;
+  line-height: 2.25rem /* 36px */;
+  margin-bottom: 0.5rem /* 8px */;
+}
+h3 {
+  margin-left: 2rem;
+}
+</style>>
 
 <script>
 export default {}
 </script>
-
-<style>
-</style>
