696b237a7353e0faa618bf3571b037516dbe1df8
CodyAdam <cody.ad4@gmail.com>
2021-04-09 13:52:42 +0200

    Add span and bilan color to blue

--- components/bilanPanel.vue
+++ components/bilanPanel.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="outer bg-white p-5 rounded-3xl shadow-2xl">
+  <div class="outer p-5 rounded-3xl h-60 sticky font-light shadow-2xl">
     <slot />
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 700;
-  font-size: 1.875rem /* 30px */;
-  line-height: 2.25rem /* 36px */;
-  margin-bottom: 0.5rem /* 8px */;
+.outer {
+  background-color: var(--blue);
+  color: white;
+  align-self: flex-start;
 }
 h3 {
   margin-left: 2rem;
+}
+
+span {
+  font-size: 1.3rem;
+  font-weight: 600;
 }
 </style>>
 
