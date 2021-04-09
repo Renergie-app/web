cc416031099d729886ec804b618d8315536ba044
CodyAdam <cody.ad4@gmail.com>
2021-04-09 10:43:34 +0200

    Fix question margin

--- components/QuestionCard.vue
+++ components/QuestionCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-col p-7 outer">
-    <div class="question text-lg font-bold mb-8">
+    <div class="question text-xl font-bold mb-5">
       {{ question }}
     </div>
     <div class="w-full flex justify-center">
@@ -14,9 +14,6 @@
   border-radius: 40px;
   background-color: var(--bg-card);
   box-shadow: 0px 5px 50px -10px rgba(49, 49, 49, 0.116);
-}
-.question {
-  margin: 5px 0;
 }
 </style>
 
